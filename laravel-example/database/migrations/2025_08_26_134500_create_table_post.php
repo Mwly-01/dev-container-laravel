13e5aabe223d514a44e5846caebddb713e8fc884
mwly-01 <mlzardila@gmail.com>
2025-08-26 13:57:05 +0000

    se temrino de terminar

--- laravel-example/database/migrations/2025_08_26_134500_create_table_post.php
+++ laravel-example/database/migrations/2025_08_26_134500_create_table_post.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('table_post', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->boolean('status');
             $table->timestamps();
         });
     }
