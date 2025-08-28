f223fe63f61b427c8406f6424a5ecdde92c8e7dc
mwly-01 <mlzardila@gmail.com>
2025-08-28 14:00:34 +0000

    feat: :sparkles: relacion complicada

--- laravel-example/app/Models/Post.php
+++ laravel-example/app/Models/Post.php
@@ -4,11 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, softDeletes;
     protected $table = "posts";
 
-    protected $fillable = ['title', 'content','status'];
+    protected $fillable = ['title', 'content','status','published_at','tags','meta'];
+
+    protected $casts = [
+        'published_at' => 'datetime', 
+        'tags'=>'array',
+        'meta'=>'meta'
+    ];
+
+    public function categories(){
+        //tabla private post_categories
+        return $this->belongsToMany(Category::class)->using(CategoryPost::class)->withTimestamps(); 
+}
 }
